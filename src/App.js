7820d99d95adee094236cd2e6dc2ef95e2a2b571
Luke Chambers <lukenatchambers@gmail.com>
2018-05-25 17:35:18 -0600

    Start styling on the landing page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <section className="App-container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </section>
     );
   }
 }
